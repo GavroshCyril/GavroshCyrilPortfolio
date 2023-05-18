9f5cc9015531e6f7badeff2124ab1b21591e64e5
Cyril <gavrosh.cyril@gmail.com>
2023-05-18 13:36:59 +0300

    feat: init project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './App.css';
+import Sidebar from './components/sidebar/Sidebar';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Services from './components/services/Services';
+import Resume from './components/resume/Resume';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
+import Blog from './components/blog/Blog';
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+      <main>
+        <Home />
+        <About />
+        <Services />
+        <Resume />
+        <Portfolio />
+        <Contact />
+        <Blog />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './contact.css'
+
+function Contact() {
+  return (
+    <div>ContactF
+      <h1>sdfs</h1>
+  <main></main>
+    </div>
+
+  )
+}
+
+export default Contact
